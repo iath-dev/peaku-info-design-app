6366d90002a7150a4e2c36003485f4f641219095
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2023-08-30 19:02:24 -0500

    Init

---
+++ src/components/responsiveDrawer.ts
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material';
+
+interface Props {
+  children: any
+}
+
+export const ResponsiveDrawer: React.FC<Props> = ({ children }) => {
+  return(
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default ResponsiveDrawer
